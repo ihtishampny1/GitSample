3695a72f04a6a9a8f8bd8a0030ec0acb4c336705
ihtishampny1 <ihtisham.pny@gmail.com>
2022-02-22 18:45:55 +0500

    My First Code Update

---
+++ app/src/main/java/it/tec/gitsample/MainActivity.java
@@ -0,0 +1,14 @@
+package it.tec.gitsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
